b0f69c907ac21627f1c20ca12a65da66ac66241f
Campos Lahoz Jose-M <o7586t@PCXXXXX.red.zaragoza.es>
2014-09-29 13:52:18 +0200

    He arreglado el problema de la 'ñ' en el fuente. Ahora sale bien por pantalla. He añadido al principio la línea:
    \#encoding: utf-8
    
    y aparentemente ya está solucionado el problema.

--- semanaAño.rb
+++ semanaAño.rb
@@ -1,6 +1,8 @@
+#encoding: utf-8
+
 require "date"
 
 time = Time.new
 
 puts " "
-puts "Estamos en la semana " + (time.strftime("%W").to_i + 1).to_s + " de este año."
+puts "Estamos en la semana " + (time.strftime("%W").to_i + 1).to_s + " de este aÃ±o."
